b753beefba20458489e83aa851be117764f6c9d2
Alex Reinoso <ralexjessiel@gmail.com>
2022-02-21 21:12:04 -0500

    finished with the main functionality

--- functions.py
+++ functions.py
@@ -1,8 +1,6 @@
 
 # Here the file is read line by line
 # as well as a bit of cleaning
-import re
-from datetime import datetime
 
 
 def readFile(path):
@@ -11,7 +9,7 @@ def readFile(path):
         lines = f.readlines()
     for l in lines:
         ln = l.replace("=", ",")
-        # .strip to get rid of newline characters
+        ln = ln.replace(" ", "")
         ln = ln.strip()
         # here is to split all the values separated by commas
         ln = ln.split(',')[0:]
@@ -22,40 +20,32 @@ def readFile(path):
 def dataToDictionary(lst):
     resDict = dict()
     for l in lst:
-        # the first value is used as the key, and everything after is passed
-        # as a list of values
         resDict[l[0]] = l[1:]
     return resDict
 
 
 fe = readFile('sample.txt')
 dataDict = dataToDictionary(fe)
-iterador = iter(dataDict)
 
-
+contador = 0
 for i in range(0, len(fe)):
-    contador = 0
-    for j in range(i+1, len(fe)):
+    for j in range(int(i+1), len(fe)):
         arrA = fe[i][1:]
         arrB = fe[j][1:]
         for e in arrA:
-            # obtener el intervalo de horas
-            #print('intervalo hora: ', e[2:])
-            # obtener el dia
             for e2 in arrB:
-                # check if the day is the same to proceed with checking if they've been
-                # at the office the same time
-                if e[:2] == e2[:2]:
-                    print('prevWorkedHour st', e[2:4], 'prevWorkedHour lst ', e[8:10],
-                          ' nextWorkerHour ', e2[2:4], 'nextWorkerHour lst ', e2[8:10])
-    print(fe[i][:2], '-', fe[j][:1], contador)
-    contador = 0
-""""
-RENE-ASTRID: 3
-"""""
+                if (e[:2] == e2[:2]):
+                    x = range(int(e[2:4]), int(e[8:10]))
+                    y = range(int(e2[2:4]), int(e2[8:10]))
+                    xs = set(x)
+                    if xs.intersection(y):
+                        contador += 1
+        if contador > 0:
+            print(fe[i][:1], '-', fe[j][:1], contador)
+        contador = 0
 
-"""""
-ideas verificar que los rangos de
-entrada y salida o sean los mismos(en cuanto a horas)
-o que uno se encuentre dentro del rango del otro
+""""
+ASTRID-RENE: 2
+*ASTRID-ANDRES: 3
+*RENE-ANDRES: 2
 """""
